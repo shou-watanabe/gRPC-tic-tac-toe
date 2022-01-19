c0965c1eb3849a00e246f71bfb0e8915c1dbfaed
shou-watanabe <watanabeshou1520@gmail.com>
2022-01-19 16:28:41 +0900

    :sparkles: add Winner

--- game/game.go
+++ game/game.go
@@ -63,17 +63,18 @@ func (g *Game) IsGameOver() Winner {
 	return NoWin
 }
 
-// //　勝者の色を返します。引き分けの場合はNoneを返します
-// func (g *Game) Winner() Symbol {
-// 	black := g.Board.Score(Black)
-// 	white := g.Board.Score(White)
-// 	if black == white {
-// 		return None
-// 	} else if black > white {
-// 		return Black
-// 	}
-// 	return White
-// }
+// 勝者の色を返します。引き分けの場合はNoneを返します
+func (g *Game) Winner() Symbol {
+	if g.Board.IsAvailableLine(Circle) {
+		return Circle
+	}
+
+	if g.Board.IsAvailableLine(Cross) {
+		return Cross
+	}
+
+	return None
+}
 
 // 盤面を出力します
 func (g *Game) Display(me Symbol) {
